7d05e1b709e2a959b763d756f3e0e8b023b76343
Aleksey-Vasiliev <vasiliev.alekcey3d@gmail.com>
2023-05-03 11:18:04 +0300

    Project already.

--- my_website/crm/admin.py
+++ my_website/crm/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, StatusCrm, CommentCrm
 
-admin.site.register(Order)
+
+class Comment(admin.StackedInline):
+    model = CommentCrm
+    fields = ('comment_time', 'comment_text')
+    readonly_fields = ('comment_time',)
+    extra = 0
+
+
+class OrderAdm(admin.ModelAdmin):
+    list_display = ('id', 'order_status', 'order_name', 'order_phone', 'order_time')
+    list_display_links = ('id', 'order_name')
+    search_fields = ('id', 'order_name', 'order_phone', 'order_time')
+    list_filter = ('order_status',)
+    list_editable = ('order_status',)
+    list_per_page = 10
+    list_max_show_all = 100
+    fields = ('id', 'order_status', 'order_time', 'order_name', 'order_phone')
+    readonly_fields = ('id', 'order_time')
+    inlines = [Comment, ]
+
+
+admin.site.register(Order, OrderAdm)
+admin.site.register(StatusCrm)
+admin.site.register(CommentCrm)
